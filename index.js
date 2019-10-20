7c83757d945a6e7a2280989a7ca655f93208a1f3
oyoyoinoyoi <sugi010783@gmail.com>
2019-10-20 18:06:30 +0900

    ハンバーガー

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+
+
+(function($) {
+    $(function() {
+        var $header = $('#top-head');
+        // Nav Fixed
+//        $(window).scroll(function() {
+//            if ($(window).scrollTop() > 350) {
+//                $header.addClass('fixed');
+//            } else {
+//                $header.removeClass('fixed');
+//            }
+//        });
+        // Nav Toggle Button
+        $('#nav-toggle').click(function(){
+            $header.toggleClass('open');
+        });
+    });
+})(jQuery);
+
+
+
 $(function () {
 	//（１）ページの概念・初期ページを設定
 	var page = 0;
